a4d859c4d2061cfe70fcfb0fb1f52f6589aad362
Wilson <wjunior_msn@hotmail.com>
2019-07-29 15:46:34 -0300

    studying about test driven development with espresso android

--- app/src/androidTest/java/wmoney/wilson/com/wmoney/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/wmoney/wilson/com/wmoney/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package wmoney.wilson.com.wmoney
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-
+import android.app.Activity
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.*
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.core.AllOf.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +23,35 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("wmoney.wilson.com.wmoney", appContext.packageName)
+
+    lateinit var activity: Class<MainActivity>
+
+    @Before
+    fun init(){
+        activity = MainActivity::class.java
+        ActivityScenario.launch(activity)
     }
+
+
+    @Test
+    fun test_label_registry_is_displayed(){
+
+        onView(withId(R.id.add))
+                .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_button_click_and_typing_text_on_email_and_password(){
+
+        onView(withId(R.id.email))
+                .perform(typeText("wjunior_msn@hotmail.com"))
+
+        onView(withId(R.id.password))
+                .perform(typeText("12345"))
+
+        onView(withId(R.id.btnLogin))
+                .perform(click())
+
+    }
+
 }
---
+++ app/src/test/java/wmoney/wilson/com/wmoney/MainActivityTest.kt
@@ -0,0 +1,16 @@
+package wmoney.wilson.com.wmoney
+
+import android.app.Activity
+import android.content.Context
+import android.test.ActivityTestCase
+import android.test.InstrumentationTestCase
+import android.test.mock.MockContext
+import android.test.suitebuilder.annotation.LargeTest
+import junit.framework.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+class ExampleUnitTest() {
+
+
+}
